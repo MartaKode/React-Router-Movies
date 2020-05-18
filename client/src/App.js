7a0bb64e472e3538b3953b17ad6c8aed12480120
Marta Krawczyk <martaalicja113@gmail.com>
2020-05-18 17:18:17 -0400

    committing changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// importing stuff from react-dom
+import { Route, Switch, Link } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
+
+// Importing more files
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -29,6 +36,22 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+
+      {/* Adding routes */}
+
+      <Switch>
+
+        <Route path='/movies/:movieID'>
+          <Movie />
+        </Route>
+
+        <Route path='/' exact>
+          <MovieList movies={movieList} />
+        </Route>
+
+
+      </Switch>
+
     </div>
   );
 };
